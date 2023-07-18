92baa09e9ea1549da871cecb3c4b15afda90f5a4
PavaniKaragi <135801376+PavaniKaragi@users.noreply.github.com>
2023-07-18 18:23:33 +0530

    initial commit

---
+++ JQuery/main.js
@@ -0,0 +1,7 @@
+// var litems=$('#p');
+// litems.text("changed from jquery");
+// litems.css('color','violet');
+// litems.css('fontsize','18px');
+// $('#p').click(function(){
+//     console.log('hiii');
+// });
---
+++ JQuery/main1.js
@@ -0,0 +1,7 @@
+// $("li")
+//      .css("color","red")
+//       //.text('changed from jquery')
+//       .width(50);
+var newElement= $("li");
+newElement.css("color","green");
+$("#my-list").append(newElement);
