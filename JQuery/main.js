5e376015460cdda37db03eea7de973ce167d59b7
PavaniKaragi <135801376+PavaniKaragi@users.noreply.github.com>
2023-07-19 16:09:25 +0530

    Delete JQuery directory

--- JQuery/main.js
+++
@@ -1,7 +0,0 @@
-// var litems=$('#p');
-// litems.text("changed from jquery");
-// litems.css('color','violet');
-// litems.css('fontsize','18px');
-// $('#p').click(function(){
-//     console.log('hiii');
-// });
